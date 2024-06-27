5d94f6b4a67f1cc949da484242a8c65e64fff9ce
Yashi Gupta <yashi.g23csai@nst.rishihood.edu.in>
2024-06-27 18:58:47 +0530

    show more and show less and checkbox ui fixes

--- src/components/FundingBy.jsx
+++ src/components/FundingBy.jsx
@@ -4,9 +4,14 @@ import React, { useEffect, useState } from 'react';
 const categories = dummyData.map(item => item.funding_by);
 const uniqueCategories = [...new Set(categories)];
 
-const FundingByDropdownMenu = React.forwardRef(({ selectedFunders, setSelectedFunders , setFunderName}, ref) => {
+const FundingByDropdownMenu = React.forwardRef(({ selectedFunders, setSelectedFunders, setFunderName }, ref) => {
   const [dropdownStyle, setDropdownStyle] = useState({});
-  const [tempselectedFunders, setTempSelectedFunders] = useState([...selectedFunders]);
+  const [tempSelectedFunders, setTempSelectedFunders] = useState([...selectedFunders]);
+  const [isShow, setIsShow] = useState(false);
+
+  const toggleShow = () => {
+    setIsShow(!isShow);
+  };
 
   const handleOptionChange = (event) => {
     const { checked, value } = event.target;
@@ -19,11 +24,21 @@ const FundingByDropdownMenu = React.forwardRef(({ selectedFunders, setSelectedFu
     });
   };
 
+  const handleItemClick = (value) => {
+    setTempSelectedFunders(prevOptions => {
+      if (prevOptions.includes(value)) {
+        return prevOptions.filter(option => option !== value);
+      } else {
+        return [...prevOptions, value];
+      }
+    });
+  };
+
   useEffect(() => {
     const updatePosition = () => {
-      const fundingbyBtn = document.getElementById('fundingbyBtn');
-      if (fundingbyBtn) {
-        const { top, left } = fundingbyBtn.getBoundingClientRect();
+      const fundBtn = document.getElementById('fundingbyBtn');
+      if (fundBtn) {
+        const { top, left } = fundBtn.getBoundingClientRect();
         setDropdownStyle({
           top: `${top + window.scrollY + 31}px`,
           left: `${left + window.scrollX}px`,
@@ -35,20 +50,20 @@ const FundingByDropdownMenu = React.forwardRef(({ selectedFunders, setSelectedFu
     window.addEventListener('resize', updatePosition);
     return () => window.removeEventListener('resize', updatePosition);
   }, []);
-  
+
   const handleClearAll = () => {
-    setTempSelectedFunders([])
+    setTempSelectedFunders([]);
   };
 
   const applySelections = () => {
-    setSelectedFunders([...tempselectedFunders]);
+    setSelectedFunders([...tempSelectedFunders]);
   };
 
   useEffect(() => {
     console.log(selectedFunders);
   }, [selectedFunders]);
 
-  if (selectedFunders.length){
+  if (selectedFunders.length) {
     const maxLength = 8;
     const funderName = selectedFunders[0];
     setFunderName(`${funderName.length > maxLength ? funderName.substring(0, maxLength) + '...' : funderName} ${selectedFunders.length > 1 ? `+ ${selectedFunders.length - 1}` : ""}`);
@@ -62,26 +77,23 @@ const FundingByDropdownMenu = React.forwardRef(({ selectedFunders, setSelectedFu
           <div className = "w-[20px] h-[20px] rounded-full text-[0.7rem] flex justify-center items-center bg-dropdown-blue text-gray-500">{selectedFunders.length}</div>
         </div>
         <ul className="flex flex-col font-sans list-none p-0 text-xs m-0 gap-0 pb-[8px]">
-        {uniqueCategories.filter(item => selectedFunders.includes(item)).map((item, index) => (
-          <li key={item + index} className="flex items-center justify-between hover:bg-gray-100 h-9 pl-[8px] pr-[8px] pt-[16px] pb-[16px] rounded-[8px] text-black">
-            {item}
-            <input type="checkbox" value={item} onChange={handleOptionChange} checked={tempselectedFunders.includes(item)} className="ml-10 checked:text-dropdown-blue" />
-          </li>
-        ))}
-        {uniqueCategories.length !== selectedFunders.length && (
-          <div className='pl-[8px] pr-[8px] pb-[7px] pt-[8px] text-gray-400'>
-            <p className = "font-semibold tracking-wider">ALL</p>
-          </div>
-        )}
-        {/* <div className='pl-[8px] pr-[8px] pb-[7px] pt-[8px] text-gray-400'>
-            <p className = "font-semibold tracking-wider">ALL</p>
-          </div> */}
-        {uniqueCategories.filter(item => !selectedFunders.includes(item)).map((item, index) => (
-          <li key={item + index} className="flex items-center justify-between hover:bg-gray-100 h-9 pl-[8px] pr-[8px] pt-[18px] pb-[18px] rounded-[8px] text-black">
-            {item}
-            <input type="checkbox" value={item} onChange={handleOptionChange} checked={tempselectedFunders.includes(item)} className="ml-10"/>
-          </li>
-        ))}
+          {uniqueCategories.filter(item => selectedFunders.includes(item)).map((item, index) => (
+            <li key={item + index} className="flex items-center justify-between hover:bg-gray-100 h-9 pl-[8px] pr-[8px] pt-[16px] pb-[16px] rounded-[8px] text-black cursor-pointer" onClick={() => handleItemClick(item)}>
+              {item}
+              <input type="checkbox" value={item} onChange={handleOptionChange} checked={tempSelectedFunders.includes(item)} className="ml-10 custom-checkbox pointer-events-none" />
+            </li>
+          ))}
+          {uniqueCategories.length !== selectedFunders.length && (
+            <div className='px-2 pb-2 pt-2 text-gray-400'>
+              <p className="font-semibold tracking-wider text-[12px] text-[#3431BB] cursor-pointer" onClick={toggleShow}>{isShow ? "SHOW LESS" : "SHOW MORE"}</p>
+            </div>
+          )}
+          {isShow && uniqueCategories.filter(item => !selectedFunders.includes(item)).map((item, index) => (
+            <li key={item + index} className="flex items-center justify-between hover:bg-gray-100 h-9 pl-[8px] pr-[8px] pt-[18px] pb-[18px] rounded-[8px] text-black cursor-pointer" onClick={() => handleItemClick(item)}>
+              {item}
+              <input type="checkbox" value={item} onChange={handleOptionChange} checked={tempSelectedFunders.includes(item)} className="ml-10 custom-checkbox pointer-events-none" />
+            </li>
+          ))}
         </ul>
         <hr />
         <div className='flex flex-row justify-between items-center rounded-[8px] pl-[8px] pr-[8px] pt-[8px] font-light text-[12px]'>
@@ -94,19 +106,20 @@ const FundingByDropdownMenu = React.forwardRef(({ selectedFunders, setSelectedFu
         </div>
       </div>
     );
-  }
-  else{
+  } else {
     setFunderName("");
     return (
       <div style={dropdownStyle} className="absolute text-[13px] text-gray-500 bg-[rgb(255,255,255)] rounded-[8px] w-auto max-w-[600px] flex flex-col whitespace-wrap z-50 pl-[12px] pr-[8px] pt-[16px] pb-[8px] border border-solid border-[#E8EAEE] font-semibold min-w-[280px]" ref={ref}>
-        <div className='pl-[8px] pr-[8px] pb-[7px]'><p className = "text-dropdown-heading">FUNDING BY</p></div>
+       <div className='pl-[8px] pr-[8px] pb-[7px]'>
+          <p className = "text-dropdown-heading">FUNDING BY</p>
+        </div>
         <ul className="flex flex-col font-sans list-none p-0 text-xs m-0 gap-0 pb-[8px]">
-        {uniqueCategories.map((item, index) => (
-          <li key={item + index} className="flex items-center justify-between hover:bg-gray-100 h-9 pl-[8px] pr-[8px] pt-[18px] pb-[18px] rounded-[8px] text-black">
-            {item}
-            <input type="checkbox" value={item} onChange={handleOptionChange} checked={tempselectedFunders.includes(item)} className="ml-10"/>
-          </li>
-        ))}
+          {uniqueCategories.map((item, index) => (
+            <li key={item + index} className="flex items-center justify-between hover:bg-gray-100 h-9 pl-[8px] pr-[8px] pt-[18px] pb-[18px] rounded-[8px] text-black cursor-pointer" onClick={() => handleItemClick(item)}>
+              {item}
+              <input type="checkbox" value={item} onChange={handleOptionChange} checked={tempSelectedFunders.includes(item)} className="ml-10 custom-checkbox pointer-events-none" />
+            </li>
+          ))}
         </ul>
         <hr />
         <div className='flex flex-row justify-between items-center rounded-[8px] pl-[8px] pr-[8px] pt-[8px] font-light text-[12px]'>
