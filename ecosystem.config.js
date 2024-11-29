2d5acfe746ba4c97fd40ae5911b66a9ecace2338
Karthikreddy0165 <Karthikredddy0165@gmail.com>
2024-11-30 03:49:56 +0530

    updated ecosytem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,16 +7,6 @@ module.exports = {
       interpreter: 'none',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000, 
+        PORT: 8000, // Change this to your actual port if different
       },
-      instances: 'max',
-      exec_mode: 'cluster',
-      max_memory_restart: '1G',
-      autorestart: true,
-      restart_delay: 1000,
-      log_date_format: 'YYYY-MM-DD HH:mm Z',
-      error_file: '/var/log/pm2/frontend-error.log',
-      out_file: '/var/log/pm2/frontend-out.log'
-    }
-  ]
-};
+}]
