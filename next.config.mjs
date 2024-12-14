723c9c32b41825c35923732c089bccaa07918ecd
soniya malviya <soniya.04malviya@gmail.com>
2024-12-14 19:36:17 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-<<<<<<< HEAD
+
     domains: ['65.0.103.91'], // Add your hostname here
-=======
+
     domains: ['3.109.208.148'], // Add your hostname here
->>>>>>> 4f8b2d1 (done with mobile responsive design, tooltip componenet)
+
   },
 };
 
