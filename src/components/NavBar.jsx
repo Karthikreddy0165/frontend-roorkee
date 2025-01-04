fa96ebb1648007a9cfda2f478c1d99741391f4ff
Karthikreddy0165 <Karthikredddy0165@gmail.com>
2025-01-04 16:42:08 +0530

    fixed build errors

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import ProfileModal from "@/pages/Modals/profileModal";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
-import { IoMenuOutline, IoMdClose } from "react-icons/io5"; // For menu icon in mobile
+import { IoMdClose } from "react-icons/io"; // For menu icon in mobile
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { useAuth } from "@/Context/AuthContext";
