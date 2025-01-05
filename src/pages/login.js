a662aa274b11dcdf80e377d7a28ced74dca5f177
soniya malviya <151515611+soniya-malviy@users.noreply.github.com>
2025-01-05 13:06:18 +0000

    vercel deployement correction

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { CiBookmark } from "react-icons/ci";
-import { FaEye, FaEyeSlash, FaSpinner, FaArrowLeftLong, FaAngleRight } from "react-icons/fa";
+import { FaEye, FaEyeSlash, FaSpinner } from "react-icons/fa";
+import {FaArrowLeftLong} from "react-icons/fa6"
 import { useAuth } from "@/Context/AuthContext";
 import loginperson from "../assets/image.png";
 
