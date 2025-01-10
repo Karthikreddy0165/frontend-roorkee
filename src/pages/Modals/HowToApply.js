f82e1d818abc68f8643ac30c1144bfe727c45f36
soniya malviya <151515611+soniya-malviy@users.noreply.github.com>
2025-01-10 09:27:15 +0530

    Update HowToApply.js

--- src/pages/Modals/HowToApply.js
+++ src/pages/Modals/HowToApply.js
@@ -7,7 +7,7 @@ import { MdClose } from "react-icons/md";
 const HowToApply = ({ closeModal }) => {
   const [currentStep, setCurrentStep] = useState(1);
   const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
-    console.log(image01)
+   
   const steps = [
     {
       title: "Step 1",
