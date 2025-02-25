051dd35880636ed34d504c687630d8d39cc7a917
Karthik Reddy <Karthikreddy0165@gmail.com>
2025-02-25 10:30:02 +0530

    Update cypress.config.mjs

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -5,7 +5,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000', 
     env: {
-      apiUrl: process.env.NEXT_PUBLIC_API_BASE_URL
+      apiUrl: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://43.204.236.103:8000',
     },
     setupNodeEvents(on, config) {
       // Add any necessary node event listeners here
