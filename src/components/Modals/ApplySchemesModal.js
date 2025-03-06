f645833e9c23d96aa751abdc0f217841a4be6ca3
soniya malviya <soniya.04malviya@gmail.com>
2025-03-07 00:16:12 +0530

    added logo in feedback and report form

--- src/components/Modals/ApplySchemesModal.js
+++ src/components/Modals/ApplySchemesModal.js
@@ -473,22 +473,187 @@ const handleReportSubmit = async (e) => {
            {/* Report Modal */}
       {reportModalOpen && (
         <div className="fixed inset-0 z-60 bg-gray-700 bg-opacity-50 flex justify-center items-center">
-        
+       
   
 
-        <div className="relative bg-white p-8 rounded-lg w-[80%] sm:w-[60%] lg:w-[40%]">
-  <button
+        <div className="relative bg-white rounded-lg w-[80%] sm:w-[60%] lg:w-[40%]">
+        <div className="bg-[#EEEEFF] lg:p-3 p-2 text-black flex items-center justify-between shadow-md">
+                  {/* Left Section: Logo & Launchpad */}
+                  <div className="flex items-center ">
+                    <div className="sm:text-[16px] mt-2 font-semibold text-[#3431BB] text-sm hover:text-blue-700 cursor-pointer flex items-center  gap-2 ">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="50"
+                        height="50"
+                        viewBox="0 0 350 350"
+                        fill="none"
+                      >
+                        <line
+                          x1="178"
+                          y1="224.048"
+                          x2="178"
+                          y2="182"
+                          stroke="white"
+                          stroke-width="40"
+                        />
+                        <rect width="350" height="350" fill="#EEEEFF" />
+                        <path
+                          d="M295.5 250.878C295.5 254.069 293.524 258.023 287.813 262.439C282.176 266.797 273.675 270.96 262.679 274.556C240.746 281.729 210.1 286.257 176 286.257C141.9 286.257 111.254 281.729 89.3212 274.556C78.3248 270.96 69.824 266.797 64.1873 262.439C58.4763 258.023 56.5 254.069 56.5 250.878C56.5 247.688 58.4763 243.733 64.1873 239.318C69.824 234.96 78.3248 230.797 89.3212 227.201C111.254 220.028 141.9 215.5 176 215.5C210.1 215.5 240.746 220.028 262.679 227.201C273.675 230.797 282.176 234.96 287.813 239.318C293.524 243.733 295.5 247.688 295.5 250.878Z"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <path
+                          d="M295.745 189.052C296.816 172.185 294.51 155.271 288.97 139.35C283.43 123.429 274.772 108.836 263.528 96.4695C252.284 84.1026 238.691 74.2221 223.585 67.435C208.479 60.6479 192.178 57.0975 175.684 57.002C159.19 56.9064 142.851 60.2677 127.671 66.8794C112.491 73.491 98.7902 83.2134 87.4105 95.4492C76.0307 107.685 67.2122 122.176 61.4967 138.032C55.7812 153.888 53.2893 170.774 54.1743 187.653L66.2569 186.987C65.4604 171.797 67.7031 156.599 72.847 142.329C77.991 128.058 85.9276 115.017 96.1694 104.004C106.411 92.992 118.742 84.2419 132.404 78.2914C146.066 72.341 160.771 69.3158 175.616 69.4018C190.46 69.4878 205.131 72.6831 218.726 78.7915C232.322 84.8999 244.555 93.7923 254.675 104.923C264.795 116.053 272.587 129.186 277.573 143.515C282.559 157.844 284.634 173.066 283.67 188.247L295.745 189.052Z"
+                          fill="#3330BA"
+                        />
+                        <line
+                          x1="54"
+                          y1="192.5"
+                          x2="126"
+                          y2="192.5"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <line
+                          x1="231"
+                          y1="192.5"
+                          x2="296"
+                          y2="192.5"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <line
+                          x1="127.5"
+                          y1="184"
+                          x2="127.5"
+                          y2="150"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <line
+                          x1="127.5"
+                          y1="198"
+                          x2="127.5"
+                          y2="156"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <path
+                          d="M151 191.042L181.369 159.618"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <line
+                          x1="180.712"
+                          y1="166.288"
+                          x2="204.754"
+                          y2="190.329"
+                          stroke="#3330BA"
+                          stroke-width="10.5"
+                        />
+                        <line
+                          x1="188.304"
+                          y1="249.66"
+                          x2="277.037"
+                          y2="267.987"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <line
+                          x1="74.8876"
+                          y1="268.773"
+                          x2="167.66"
+                          y2="249.614"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <line
+                          x1="163.5"
+                          y1="255"
+                          x2="163.5"
+                          y2="223"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <line
+                          x1="192.5"
+                          y1="255"
+                          x2="192.5"
+                          y2="223"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <line
+                          x1="178.501"
+                          y1="285.089"
+                          x2="177.501"
+                          y2="223.089"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <line
+                          x1="203.5"
+                          y1="221"
+                          x2="203.5"
+                          y2="187"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <line
+                          x1="152.5"
+                          y1="221"
+                          x2="152.5"
+                          y2="187"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <path d="M226.5 198V155" stroke="#3330BA" stroke-width="11" />
+                        <line
+                          x1="125.505"
+                          y1="154.016"
+                          x2="182.505"
+                          y2="104.016"
+                          stroke="#3330BA"
+                          stroke-width="10.6"
+                        />
+                        <line
+                          x1="179.854"
+                          y1="109.177"
+                          x2="228.204"
+                          y2="159.177"
+                          stroke="#3330BA"
+                          stroke-width="11"
+                        />
+                        <line
+                          x1="178"
+                          y1="205"
+                          x2="178"
+                          y2="222"
+                          stroke="#EEEEFF"
+                          stroke-width="40"
+                        />
+                      </svg>
+        
+                      <div className="mt-1">LAUNCHPAD</div>
+                    </div>
+                    <button
     type="button"  
     onClick={() => setReportModalOpen(false)}  
-    className="absolute top-1 right-1 flex-shrink-0 px-4 py-2 rounded-lg border border-transparent  text-gray-500 text-[12px] sm:text-sm"
+    className="absolute top-5 right-1 flex-shrink-0 px-4 py-2 rounded-lg border border-transparent  text-gray-500 text-[12px] sm:text-sm"
   >
     <IoMdClose className="w-[24px] h-[24px]" />
   </button>
+                  </div>
+        
+                  {/* Center Section: Profile */}
+        
+                  {/* Right Section: Close Button */}
+                </div>
 
   <h2 className="text-xl font-bold flex items-center justify-center  text-[#3330BA] p-[1rem] text-center">
     Help Us Correct Scheme Information
   </h2>
-            <form onSubmit={handleReportSubmit} className="space-y-4 ">
+            <form onSubmit={handleReportSubmit} className="space-y-4 lg:p-[3rem] p-[1rem] ">
             <div className="pt-[1rem] pb-[1rem]">
                 <label className="block text-[13px] font-semibold mb-2">Select the category</label>
                 <select
--- src/components/feedBack.jsx
+++ src/components/feedBack.jsx
@@ -61,7 +61,7 @@ const FeedbackModal = ({ isOpen, onRequestClose }) => {
         requestOptions
       );
 
-      if (!response.ok){
+      if (!response.ok) {
         throw new Error("Failed to submit feedback.");
       }
 
@@ -69,15 +69,21 @@ const FeedbackModal = ({ isOpen, onRequestClose }) => {
       setRating(0);
 
       // Success toast notification
-      toast.success("Feedback recieved successfully", { position: "top-right", autoClose: 3000 });
+      toast.success("Feedback recieved successfully", {
+        position: "top-right",
+        autoClose: 3000,
+      });
 
       setReportFormData({ category: "", description: "" });
       onRequestClose();
-    } catch (err){
-          // Error toast notification
-    toast.error("Failed to give feedback. Please try again later.", { position: "top-right", autoClose: 3000 });
-    onRequestClose();
-    } finally{
+    } catch (err) {
+      // Error toast notification
+      toast.error("Failed to give feedback. Please try again later.", {
+        position: "top-right",
+        autoClose: 3000,
+      });
+      onRequestClose();
+    } finally {
       setLoading(false);
     }
   };
@@ -88,13 +94,178 @@ const FeedbackModal = ({ isOpen, onRequestClose }) => {
 
   return (
     <div className="fixed inset-0 z-50 bg-gray-800 bg-opacity-50 flex justify-center items-center">
-      <div className="relative bg-white p-8 rounded-lg w-[80%] max-w-md">
-        <button
-          className="absolute top-0 right-0 p-[18px] text-lg hover:text-red-500"
-          onClick={onRequestClose}
-        >
-          <IoMdClose className="w-[24px] h-[24px]" />
-        </button>
+      <div className="relative bg-white rounded-lg w-[80%] max-w-md">
+        <div className="bg-[#EEEEFF] p-3 text-black flex items-center justify-between shadow-md">
+          {/* Left Section: Logo & Launchpad */}
+          <div className="flex items-center ">
+            <div className="sm:text-[16px] mt-2 font-semibold text-[#3431BB] text-sm hover:text-blue-700 cursor-pointer flex items-center  gap-2 ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="50"
+                height="50"
+                viewBox="0 0 350 350"
+                fill="none"
+              >
+                <line
+                  x1="178"
+                  y1="224.048"
+                  x2="178"
+                  y2="182"
+                  stroke="white"
+                  stroke-width="40"
+                />
+                <rect width="350" height="350" fill="#EEEEFF" />
+                <path
+                  d="M295.5 250.878C295.5 254.069 293.524 258.023 287.813 262.439C282.176 266.797 273.675 270.96 262.679 274.556C240.746 281.729 210.1 286.257 176 286.257C141.9 286.257 111.254 281.729 89.3212 274.556C78.3248 270.96 69.824 266.797 64.1873 262.439C58.4763 258.023 56.5 254.069 56.5 250.878C56.5 247.688 58.4763 243.733 64.1873 239.318C69.824 234.96 78.3248 230.797 89.3212 227.201C111.254 220.028 141.9 215.5 176 215.5C210.1 215.5 240.746 220.028 262.679 227.201C273.675 230.797 282.176 234.96 287.813 239.318C293.524 243.733 295.5 247.688 295.5 250.878Z"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <path
+                  d="M295.745 189.052C296.816 172.185 294.51 155.271 288.97 139.35C283.43 123.429 274.772 108.836 263.528 96.4695C252.284 84.1026 238.691 74.2221 223.585 67.435C208.479 60.6479 192.178 57.0975 175.684 57.002C159.19 56.9064 142.851 60.2677 127.671 66.8794C112.491 73.491 98.7902 83.2134 87.4105 95.4492C76.0307 107.685 67.2122 122.176 61.4967 138.032C55.7812 153.888 53.2893 170.774 54.1743 187.653L66.2569 186.987C65.4604 171.797 67.7031 156.599 72.847 142.329C77.991 128.058 85.9276 115.017 96.1694 104.004C106.411 92.992 118.742 84.2419 132.404 78.2914C146.066 72.341 160.771 69.3158 175.616 69.4018C190.46 69.4878 205.131 72.6831 218.726 78.7915C232.322 84.8999 244.555 93.7923 254.675 104.923C264.795 116.053 272.587 129.186 277.573 143.515C282.559 157.844 284.634 173.066 283.67 188.247L295.745 189.052Z"
+                  fill="#3330BA"
+                />
+                <line
+                  x1="54"
+                  y1="192.5"
+                  x2="126"
+                  y2="192.5"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <line
+                  x1="231"
+                  y1="192.5"
+                  x2="296"
+                  y2="192.5"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <line
+                  x1="127.5"
+                  y1="184"
+                  x2="127.5"
+                  y2="150"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <line
+                  x1="127.5"
+                  y1="198"
+                  x2="127.5"
+                  y2="156"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <path
+                  d="M151 191.042L181.369 159.618"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <line
+                  x1="180.712"
+                  y1="166.288"
+                  x2="204.754"
+                  y2="190.329"
+                  stroke="#3330BA"
+                  stroke-width="10.5"
+                />
+                <line
+                  x1="188.304"
+                  y1="249.66"
+                  x2="277.037"
+                  y2="267.987"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <line
+                  x1="74.8876"
+                  y1="268.773"
+                  x2="167.66"
+                  y2="249.614"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <line
+                  x1="163.5"
+                  y1="255"
+                  x2="163.5"
+                  y2="223"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <line
+                  x1="192.5"
+                  y1="255"
+                  x2="192.5"
+                  y2="223"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <line
+                  x1="178.501"
+                  y1="285.089"
+                  x2="177.501"
+                  y2="223.089"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <line
+                  x1="203.5"
+                  y1="221"
+                  x2="203.5"
+                  y2="187"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <line
+                  x1="152.5"
+                  y1="221"
+                  x2="152.5"
+                  y2="187"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <path d="M226.5 198V155" stroke="#3330BA" stroke-width="11" />
+                <line
+                  x1="125.505"
+                  y1="154.016"
+                  x2="182.505"
+                  y2="104.016"
+                  stroke="#3330BA"
+                  stroke-width="10.6"
+                />
+                <line
+                  x1="179.854"
+                  y1="109.177"
+                  x2="228.204"
+                  y2="159.177"
+                  stroke="#3330BA"
+                  stroke-width="11"
+                />
+                <line
+                  x1="178"
+                  y1="205"
+                  x2="178"
+                  y2="222"
+                  stroke="#EEEEFF"
+                  stroke-width="40"
+                />
+              </svg>
+
+              <div className="mt-1">LAUNCHPAD</div>
+            </div>
+            <button
+              className="absolute top-3 right-0 p-[18px] text-lg hover:text-red-500"
+              onClick={onRequestClose}
+            >
+              <IoMdClose className="w-[24px] h-[24px]" />
+            </button>
+          </div>
+
+          {/* Center Section: Profile */}
+
+          {/* Right Section: Close Button */}
+        </div>
         <h2 className="text-[20px] text-[#3330BA] font-bold mb-4 text-center mt-[1rem]">
           Feedback Form
         </h2>
@@ -121,7 +292,7 @@ const FeedbackModal = ({ isOpen, onRequestClose }) => {
         </div>
 
         {/* Feedback Form */}
-        <div className="mb-4 pt-[1rem] pb-[1rem]">
+        <div className="mb-4 pt-[1rem] px-5 pb-[1rem]">
           <label className="block text-[13px] text-[#000000] font-semibold mb-2">
             Select the category
           </label>
@@ -139,7 +310,7 @@ const FeedbackModal = ({ isOpen, onRequestClose }) => {
           </select>
         </div>
 
-        <div className="mb-4">
+        <div className="mb-4 px-5">
           <label className="block text-[13px] text-[#000000] font-semibold mb-2">
             Please write your feedback below. 
           </label>
@@ -155,7 +326,7 @@ const FeedbackModal = ({ isOpen, onRequestClose }) => {
         </div>
 
         {/* Submit and Cancel Buttons */}
-        <div className="flex justify-center items-centre h-full">
+        <div className="flex justify-center items-centre p-5 h-full">
           <button
             onClick={handleSubmitFeedback}
             className=" px-4 py-2 rounded-lg text-[13px] bg-[#3330BA] text-white hover:bg-blue-700"
