17dc140dff849f2eeede1f3d26ce573a73b571f5
Karthikreddy0165 <Karthikredddy0165@gmail.com>
2025-03-19 00:03:25 +0530

    added test for image visibilty

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -8,7 +8,6 @@ export default defineConfig({
       apiUrl: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://43.204.236.103:8000',
     },
     setupNodeEvents(on, config) {
-      console.log("Using API URL:", config.env.apiUrl);
       // Add any necessary node event listeners here
     },
   },
